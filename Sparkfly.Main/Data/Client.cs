faa9584ce811f83527221184369c652ffb0cad7e
BuenoVini <vsbueno38@gmail.com>
2022-09-18 15:27:15 -0300

    client is now saved on the welcome page

--- Sparkfly.Main/Data/Client.cs
+++ Sparkfly.Main/Data/Client.cs
@@ -3,9 +3,9 @@
 public class Client
 {
     public string Id { get; private set; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
-    public Client(string id, string? name)  // NOTE: try to remove this constructor, may be unecessary
+    public Client(string id, string name)  // NOTE: try to remove this constructor, may be unecessary
     {
         Id = id;
         Name = name;
