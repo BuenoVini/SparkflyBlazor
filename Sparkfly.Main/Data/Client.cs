3fc521a323445ad784e94f53308dc435b80987ff
BuenoVini <vsbueno38@gmail.com>
2022-10-15 19:31:54 -0300

    hosts can now remove other peoples votes

--- Sparkfly.Main/Data/Client.cs
+++ Sparkfly.Main/Data/Client.cs
@@ -4,10 +4,12 @@ public class Client
 {
     public string Id { get; private set; }
     public string Name { get; set; }
+    public bool IsHost { get; set; }
 
-    public Client(string id, string name)  // NOTE: try to remove this constructor, may be unecessary
+    public Client(string id, string name, bool isHost = false)  // NOTE: try to remove this constructor, may be unecessary
     {
         Id = id;
         Name = name;
+        IsHost = isHost;
     }
 }
